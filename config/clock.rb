f5a5a18103d97c1fef0a20efa73618026a94005d
Ryan T. Hosford <tad.hosford@gmail.com>
2017-02-25 23:02:02 -0600

    no delay

--- config/clock.rb
+++ config/clock.rb
@@ -5,7 +5,7 @@ require_relative "environment"
 module Clockwork
   every(1.minute, "onboarding_messages.send") do
     puts "Sending onboarding messages"
-    OnboardingMessageSender.new.delay.run
+    OnboardingMessageSender.new.run
   end
 
   # every(1.hour, "quarterly_messages.send") do
@@ -15,11 +15,11 @@ module Clockwork
 
   every(1.day, "employees.import", at: "03:00", tz: "UTC") do
     puts "Importing new employees"
-    EmployeeImporter.new.delay.run
+    EmployeeImporter.new.run
   end
 
   every(1.day, "employees.update", at: "03:00", tz: "UTC") do
     puts "Updating employee slack usernames"
-    EmployeeUpdater.new.delay.run
+    EmployeeUpdater.new.run
   end
 end
